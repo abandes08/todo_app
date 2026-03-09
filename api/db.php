045b148d207c8d40072aa450573b688461ad0b40
abandes08 <andesanthony08@gmail.com>
2026-03-10 00:20:52 +0800

    Add MySQL database connection

---
+++ api/db.php
@@ -0,0 +1,15 @@
+<!-- Database Connection -->
+<?php
+
+$host = 'localhost';
+$username = 'pma_user';
+$password = 'StrongPMAPassword123!';
+$dbname = 'todo_db';
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
